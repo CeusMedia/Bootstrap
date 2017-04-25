7547a3e38dd680f54126b116120a945f6299b869
kriss0r <kriss@ceusmedia.de>
2017-04-25 04:03:09 +0200

    Add legacy classes.

---
+++ src/ButtonGroup.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *	Legacy class.
+ *	@category		Library
+ *	@package		CeusMedia_Bootstrap
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2012-2015 {@link http://ceusmedia.de/ Ceus Media}
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/Bootstrap
+ */
+namespace CeusMedia\Bootstrap;
+/**
+ *	Legacy class.
+ *	@category		Library
+ *	@package		CeusMedia_Bootstrap
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2012-2015 {@link http://ceusmedia.de/ Ceus Media}
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			https://github.com/CeusMedia/Bootstrap
+ */
+class ButtonGroup extends \CeusMedia\Bootstrap\Button\Group{
+}
