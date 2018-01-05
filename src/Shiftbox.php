c853c0d4b4e58362bc6a3040f094ee921b253de1
Christian Würker <kriss@ceusmedia.de>
2018-01-05 16:42:44 +0100

    Add Shiftbox and updated Checkbox.

---
+++ src/Shiftbox.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *	Replacement for checkbox inputs.
+ *	@category		Library
+ *	@package		CeusMedia_Bootstrap
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2013 {@link http://ceusmedia.de/ Ceus Media}
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			http://code.google.com/p/cmmodules/
+ *	@since			0.3.0
+ *	@version		$Id$
+ *	@link			https://github.com/nostalgiaz/bootstrap-switch	requires Bootstrap Switch URL description
+ *	@see			http://www.larentis.eu/switch/					original examples
+ *	@see			http://bdmdesign.github.io/bootstrap-switch-BdMdesigN/examples.html		latest examples
+ */
+namespace CeusMedia\Bootstrap;
+/**
+ *	Replacement for checkbox inputs.
+ *	@category		Library
+ *	@package		CeusMedia_Bootstrap
+ *	@author			Christian Würker <christian.wuerker@ceusmedia.de>
+ *	@copyright		2013 {@link http://ceusmedia.de/ Ceus Media}
+ *	@license		http://www.gnu.org/licenses/gpl-3.0.txt GPL 3
+ *	@link			http://code.google.com/p/cmmodules/
+ *	@since			0.3.0
+ *	@version		$Id$
+ */
+class Shiftbox extends Component{
+
+	const SIZE_DEFAULT		= "";
+	const SIZE_LARGE		= "large";
+	const SIZE_MINI			= "mini";
+	const SIZE_SMALL		= "small";
+
+	protected $name;
+	protected $value;
+	protected $options;
+	protected $size;
+
+	public function __construct( $name = NULL, $value = NULL, $checked = NULL, $data = array() ){
+		$this->setName( $name );
+		$this->setValue( $value );
+		$this->setChecked( $checked );
+		foreach( $data as $key => $value )
+			$this->setData( $key, $value );
+	}
+
+	public function render(){
+		$attributes	= array(
+			'type'		=> 'checkbox',
+			'id'		=> 'input_'.$this->name,
+			'name'		=> $this->name,
+			'value'		=> $this->value,
+			'class'		=> 'shiftbox',
+			'checked'	=> $this->checked ? "checked" : NULL,
+		);
+		$this->extendAttributesByData( $attributes );
+		$input			= \UI_HTML_Tag::create( 'input', NULL, $attributes );
+		return $input;
+		$attributes	= array();
+		$attributes['class']	= "make-switch";
+//		$this->extendAttributesByData( $attributes );
+		return \UI_HTML_Tag::create( 'div', $input, $attributes );
+	}
+
+	public function setChecked( $checked ){
+		$this->checked	= $checked;
+	}
+
+	public function setName( $name ){
+		$this->name		= $name;
+		$this->setId( $name ? 'input_'.$name : "" );
+	}
+
+	public function setValue( $value ){
+		$this->value	= htmlentities( $value, ENT_QUOTES, 'UTF-8' );
+	}
+}
+?>
