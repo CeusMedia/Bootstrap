94d2e6e6bc0d3ee6f2b3bbde0f01cfbd304a9ed4
Christian Würker <kriss@ceusmedia.de>
2018-01-05 04:06:28 +0100

    Extend Code component by tab to whitespace replacement.

--- src/Code.php
+++ src/Code.php
@@ -20,12 +20,16 @@ namespace CeusMedia\Bootstrap;
  */
 class Code extends Component{
 
-	protected $scrollable		= FALSE;
+	protected $convertTabsToWhitespace	= TRUE;
+	protected $scrollable				= FALSE;
 
-	public function __construct( $content, $scrollable = FALSE, $class = NULL ){
+	static public $tabSize				= 4;
+
+	public function __construct( $content, $scrollable = FALSE, $class = NULL, $convertTabsToWhitespace = TRUE ){
 		$this->setContent( $content );
 		$this->setClass( $class );
 		$this->setScrollable( $scrollable );
+		$this->convertTabsToWhitespace	= $convertTabsToWhitespace;
 	}
 
 	public function setScrollable( $scrollable ){
@@ -36,7 +40,26 @@ class Code extends Component{
 		$attributes		= array( 'class' => join( " ", $this->class ) );
 		if( $this->scrollable )
 			$attributes['class']	.= " pre-scrollable";
-		return \UI_HTML_Tag::create( 'pre', htmlentities( $this->content, ENT_QUOTES, 'UTF-8' ), $attributes );
+		$content	= $this->content;
+		if( $this->convertTabsToWhitespace )
+			$content	= $this->convertTabsToWhitespace( $this->content );
+		return \UI_HTML_Tag::create( 'pre', htmlentities( $content, ENT_QUOTES, 'UTF-8' ), $attributes );
+	}
+
+	protected function convertTabsToWhitespace( $content ){
+		$lines	= array();
+		foreach( explode( "\n", $content ) as $line ){
+			$line	= trim( $line, "\r" );
+			while( substr_count( $line, "\t" ) ){
+				$pos	= strpos( $line, "\t" );
+				$indent	= static::$tabSize - ( $pos % static::$tabSize );
+				$subst	= str_repeat( " ", $indent );
+//				$line	= substr( $line, 0, $pos ).$subst.substr( $line, $pos + 1 );
+				$line	= preg_replace( "/\t/", $subst, $line, 1 );
+			}
+			$lines[]	= $line;
+		}
+		return join( "\n", $lines );
 	}
 }
 ?>
